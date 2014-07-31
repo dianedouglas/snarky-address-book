7d451e57dd98c682d4ae570d3155e9433770fef7
Diane Douglas & Lee Kebler <student@epicodus.com>
2014-07-31 09:55:08 -0700

    will create attributes for the person class that include email, phone, and address

--- lib/person.rb
+++ lib/person.rb
@@ -1,4 +1,24 @@
 class Person
-  def initialize(name)
+  def initialize(name, phone, email, address)
+    @name = name
+    @phone= phone
+    @email = email
+    @address = address
+  end
+
+  def name
+    @name
+  end
+
+  def phone
+    @phone
+  end
+
+  def email
+    @email
+  end
+
+  def address
+    @address
   end
 end
--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -4,8 +4,28 @@ require 'person'
 
 describe Person do
   it "is initialized with a name" do
-    test_person = Person.new('Lee Kebler')
+    test_person = Person.new('Lee Kebler', 5555555555, 'keebz@keebz.com', '123 street st. Portland, OR 97209')
     test_person.should be_an_instance_of Person
+  end
+
+  it "lets your read the name of the person" do
+    test_person = Person.new('Lee Kebler', 5555555555, 'keebz@keebz.com', '123 street st. Portland, OR 97209')
+    test_person.name.should eq 'Lee Kebler'
+  end
+
+  it "lets you read the phone number of the person" do
+    test_person = Person.new('Lee Kebler', 5555555555, 'keebz@keebz.com', '123 street st. Portland, OR 97209')
+    test_person.phone.should eq 5555555555
+  end
+
+  it "lets you read the email of the person" do
+    test_person = Person.new('Lee Kebler', 5555555555, 'keebz@keebz.com', '123 street st. Portland, OR 97209')
+    test_person.email.should eq 'keebz@keebz.com'
+  end
+
+  it "lets you read the address of the person" do
+    test_person = Person.new('Lee Kebler', 5555555555, 'keebz@keebz.com', '123 street st. Portland, OR 97209')
+    test_person.address.should eq '123 street st. Portland, OR 97209'
   end
 end
 
